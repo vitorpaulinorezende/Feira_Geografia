d717eb4336b39d1300d821267c896ec54eac7265
vitorP.rezende <59585477+vitorpaulinorezende@users.noreply.github.com>
2022-11-11 17:13:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const texts = {
-	1: ["Título 1", 
-		"1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		""],
-	2: ["Título 2",
-		"2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		""],
-	3: ["Título 3",
-		"3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		""],
-	4: ["Título 4",
-		"4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		""],
+	1: ["Economia", 
+		"O PIB é o produto interno bruto representa a soma de todos os bens e serviços finais produzidos numa determinada região e durante um determinado período, o PIB é um dos indicadores mais utilizados na macroemia  com o objetivo de quantificar a atividade econômica de uma região.<br/><br/> Com 38 milhões de habitantes, A Polônia é a maior economia da Europa Central e Leste . O país , que registra um crescimento ininterrupto desde que virou uma economia de mercado a 30 anos, avançou 4,1% em 2019, após uma alta de 5,3% em 2018",
+		"./Images/graphic.png"],
+	2: ["Importação / Exportação",
+		"A importação tem em 1° lugar medicamento, em 2° lugar partes de carros como volante e etc., em 3° lugar borrachas sintéticas. Isso gera um gasto de 192 milhões de dólares. <br/><br/> A exportação tem em 1° lugar minérios de cobre, em 2° lugar farelos de soja e outros alimentos para animais, em 3° lugar motores de pistão. Esses produtos geram um ganho de 598 milhões de dólares e um lucro de 406 milhões de dólares com os gastos da importação",
+		"./Images/polonia_dados.png"],
+	3: ["Parceiros e Blocos Econômicos",
+		"Principais Parceiros de Exportação: Alemanha 28,8,Reino Unido 6,6, República Tcheca 6,4,França 6,3,Itália 5,5, países baixos 4,5, Rússia 4,5 (2011) <br/><br/> Principais Parceiros de Importação: Alemanha 26,7, Rússia 10,3, países baixos 5,9,Itália 5,3,República popular da China 5,3, França 4,4, República Tcheca 4,3(2011) <br/><br/> Blocos econômicos: OMC ,União Europeia(UE), OCDE",
+		"./Images/bloco.jpg"],
 };
 
 var selectedContent = 1;
@@ -20,7 +17,7 @@ var content = document.querySelector(".content");
 function setContent(number) {
 	content.querySelector("h1").setHTML("<h1>"+texts[number][0]+"</h1>");
 	content.querySelector("p").setHTML("<p>"+texts[number][1]+"</p>");
-	content.querySelector("img").setAttribute("src", texts[number][2])
+	content.querySelector("img").setAttribute("src", texts[number][2]);
 }
 
 function prevContent() {
